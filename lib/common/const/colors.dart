53f9772564064adf22cb98ca8497828491eac55b
nx006 <limazero14@gmail.com>
2023-09-03 22:55:08 +0900

    feat: 색상 상수 설정

---
+++ lib/common/const/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+/// 주 어두운 계열 색상
+const kPrimaryDarkColor = Color(0xFF2F2F2F);
+
+/// 텍스트 필드 색상
+const kTextFieldColor = Color(0xFF484848);
+
+/// 하이라이트 텍스트 색상
+const kHighlightTextColor = Color(0xFFB6FFCB);
+
+/// 하이라이트 색상
+const kPrimaryHighlightColor2 = Color(0xFF83D0AB);
+
+/// Secondary 하이라이트 색상 (연한 녹색)
+const kSecondaryHighlightColor = Color(0xFFB0E7C9);
+
+/// 배경 색상
+const kBackgroundColor = Color(0xFFF8F8F8);
