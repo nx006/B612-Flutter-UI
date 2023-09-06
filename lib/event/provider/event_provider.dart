fd2398c0f33d4867ba2a9b988045c54dd88fb1d8
nx006 <limazero14@gmail.com>
2023-09-06 13:41:44 +0900

    feat: EventProvider 및 Model 생성

---
+++ lib/event/provider/event_provider.dart
@@ -0,0 +1,24 @@
+import 'package:b612_flutter_ui/event/model/event_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class EventStateNotifier extends StateNotifier<List<EventModel>> {
+  EventStateNotifier() : super([..._initialState]);
+}
+
+const _initialState = <EventModel>[
+  EventModel(
+    title: '메이저리그 체육대회',
+    price: 150,
+    thumbnail: 'asset/images/event1.jpeg',
+  ),
+  EventModel(
+    title: '마이너리그 체육대회',
+    price: 150,
+    thumbnail: 'asset/images/event2.jpeg',
+  ),
+  EventModel(
+    title: '대형 재롱잔치',
+    price: 150,
+    thumbnail: 'asset/images/event3.jpeg',
+  ),
+];
